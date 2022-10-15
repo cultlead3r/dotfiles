9a4b0007c117182dcde005c036bf9e25ca5d004b
cultleader <cultleader@cultleader.me>
2022-10-15 04:07:44 -0500

    initial commit

---
+++ nvim/lua/user/lsp/settings/pyright.lua
@@ -0,0 +1,11 @@
+return {
+  settings = {
+    python = {
+      -- set python venv
+      pythonPath = "/home/cultleader/tmp/venv/bin/python",
+      analysis = {
+        typeCheckingMode = "off",
+      },
+    },
+  },
+}
